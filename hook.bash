be408d2dbb9ff3b4a5076e3ea0445503a790cf20
Alexey Melezhik <melezhik@gmail.com>
2016-06-29 15:08:59 +0300

    use uname -a

--- hook.bash
+++ hook.bash
@@ -1,9 +1,13 @@
-os=`cat /etc/*-release|grep -e ^ID=`
+os=`uname -a`
+
+set_stdout $os
+
+shopt -s nocasematch;
 
 for p in $(config list)
 do
 
-  if [[ "$os" =~ "debian" ]]; then 
+  if [[ $os =~ "debian" ]]; then 
     run_story apt-get action install package $p
   elif [[ "$os" =~ "ubuntu" ]]; then 
     run_story apt-get action install package $p
