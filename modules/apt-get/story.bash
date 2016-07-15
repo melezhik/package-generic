6cfdff442b9bb534f82598765647ca8a412d578a
melezhik <melezhik@gmail.com>
2016-07-15 13:57:16 +0300

    up!

--- modules/apt-get/story.bash
+++ modules/apt-get/story.bash
@@ -3,7 +3,7 @@ package=$(story_var package)
 echo trying to install $package ...
 echo installer - apt-get
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install $package >/dev/null
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $package
 
 dpkg -s $package | grep Package:
 dpkg -s $package | grep Version:
