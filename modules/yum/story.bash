ad036d47ee61aeab1301502c097823dfb0720a94
Alexey Melezhik <melezhik@gmail.com>
2016-08-16 14:45:39 +0300

    yum-update

--- modules/yum/story.bash
+++ modules/yum/story.bash
@@ -3,6 +3,8 @@ package=$(story_var package)
 echo trying to install $package ...
 echo installer - yum 
 
+yum makecache fast
+
 yum -y -q install $package || exit 1
 
 yum -q list installed $package || exit 1 
