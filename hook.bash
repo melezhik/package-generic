1d13dc60771121c171683ec72ee3bb03ebab3082
Alexey Melezhik <melezhik@gmail.com>
2016-08-19 13:46:18 +0300

    handle-unknown-os

--- hook.bash
+++ hook.bash
@@ -9,16 +9,21 @@ if test $action = "install"; then
   
     if [[ $os =~ "debian" ]]; then 
       run_story apt-get action install package $p
+      set_stdout package-generic-done
     elif [[ "$os" =~ "ubuntu" ]]; then 
       run_story apt-get action install package $p
+      set_stdout package-generic-done
     elif [[ "$os" =~ "centos" ]]; then
       run_story yum action install package $p
+      set_stdout package-generic-done
+    else
+      set_stdout "unknown os: $os"
     fi
   
   done
 
 elif test $action = "autoremove"; then
   run_story autoremove
+  set_stdout package-generic-done
 fi  
 
-set_stdout package-generic-done
