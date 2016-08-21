52586d15ae334aee9e41e6d958fab03aa5bbb2fb
Alexey Melezhik <melezhik@gmail.com>
2016-08-21 13:10:44 +0300

    using-meta

---
+++ hook.pl
@@ -0,0 +1,29 @@
+my $action=config()->{action};
+
+
+unless ( os() eq 'debian' or os() eq 'ubuntu' or os()=~/centos/){
+  die "unsupported os: ".os();
+}
+
+
+if ($action eq 'install'){
+
+  for my $p (split /\s/, config()->{list}){
+
+    if ( os() eq 'debian' ){
+      run_story('apt-get', { action => 'install', package => $p });
+    } elsif ( os() eq 'ubuntu'){
+      run_story('apt-get', { action => 'install', package => $p });
+    } else {
+      run_story('yum', { action => 'install', package => $p });
+    }
+  }
+
+}elsif( $action eq 'autoremove') {
+      run_story('autoremove');
+}else{
+  die "unknown action: $action";
+}
+
+
+
