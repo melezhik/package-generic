1406a09040b624bc7a1cc7714da22e4191843594
Alexey Melezhik <melezhik@gmail.com>
2016-08-26 12:04:07 +0300

    optimizing yum install

--- modules/yum/story.bash
+++ modules/yum/story.bash
@@ -3,12 +3,14 @@ package=$(story_var package)
 echo trying to install $package ...
 echo installer - yum 
 
-yum makecache fast
+if yum -q list installed $package 2>/dev/null ; then
+  yum -q list installed $package
+else
+  yum makecache fast
+  yum -y -q install $package || exit 1
+  yum -q list installed $package || exit 1 
+fi
 
-yum -y -q install $package || exit 1
-
-yum -q list installed $package || exit 1 
-
-echo "$package Installed OK"
+echo done
 
 
