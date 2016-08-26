43a249b76af10113695416ec308222e29d4f246f
Alexey Melezhik <melezhik@gmail.com>
2016-08-26 12:28:56 +0300

    apt-optimization

--- modules/apt-get/story.bash
+++ modules/apt-get/story.bash
@@ -3,7 +3,7 @@ package=$(story_var package)
 echo trying to install $package ...
 echo installer - apt-get
 
-if dpkg -s $package | grep Status: | grep 'install ok installed' 1>/dev/null 2>&1;
+if dpkg -s $package | grep Status: | grep 'install ok installed' 1>/dev/null 2>&1; then
   dpkg -s $package | grep Package:
   dpkg -s $package | grep Version:
   dpkg -s $package | grep Status:
