a98e202efc81256d3080b52830449bd6883842ec
Alexey Melezhik <melezhik@gmail.com>
2016-08-26 12:27:24 +0300

    apt-optimization

--- modules/apt-get/story.bash
+++ modules/apt-get/story.bash
@@ -3,9 +3,15 @@ package=$(story_var package)
 echo trying to install $package ...
 echo installer - apt-get
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $package
+if dpkg -s $package | grep Status: | grep 'install ok installed' 1>/dev/null 2>&1;
+  dpkg -s $package | grep Package:
+  dpkg -s $package | grep Version:
+  dpkg -s $package | grep Status:
+else
+  DEBIAN_FRONTEND=noninteractive apt-get install -y -qq $package || exit 1
+  dpkg -s $package | grep Package:
+  dpkg -s $package | grep Version:
+  dpkg -s $package | grep Status:
+fi
 
-dpkg -s $package | grep Package:
-dpkg -s $package | grep Version:
-dpkg -s $package | grep Status:
 
